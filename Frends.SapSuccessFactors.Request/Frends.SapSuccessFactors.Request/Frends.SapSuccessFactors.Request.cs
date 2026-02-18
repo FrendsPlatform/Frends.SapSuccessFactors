29700776ca01f1efdd91a8393ee596a1924e7197
MichalFrends1 <michal.nowakowski@frends.com>
2026-02-18 11:01:22 +0100

    add comment to client constructor

--- Frends.SapSuccessFactors.Request/Frends.SapSuccessFactors.Request/Frends.SapSuccessFactors.Request.cs
+++ Frends.SapSuccessFactors.Request/Frends.SapSuccessFactors.Request/Frends.SapSuccessFactors.Request.cs
@@ -19,6 +19,7 @@ public static class SapSuccessFactors
 {
     /// <summary>
     /// Factory for creating REST clients. Can be overridden in tests to provide a mock client.
+    /// Internal visibility allows test projects to access it via InternalsVisibleTo attribute.
     /// </summary>
 #pragma warning disable SA1401 // Fields should be private
     internal static Func<RestClientOptions, IRestClient> RestClientConstructor = options => new RestClient(options);
